f00cca295145c58526179981b393e6e49cc17f5d
Jcorrales07 <corralesjoe07@gmail.com>
2022-04-29 11:27:24 -0600

    codigo de prueba para SFML, se me habia olvidado JASDJASdj

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "iostream"
+#include "SFML/Graphics.hpp"
 
 using std::cout; using std::endl;
 
@@ -7,4 +8,25 @@ int main() {
 
 	cout << " Este es el main " << endl;
 	cout << "...No tocar nada... " << endl;
+
+    // prueba de SFML
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
